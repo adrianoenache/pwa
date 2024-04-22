b032606941d3e7760fb17105d3cc516722d808c0
Adriano Enache <adrianoenache@hotmail.com>
2024-04-22 15:15:52 -0300

    It was removed comments.

--- webdev/js/my-app.js
+++ webdev/js/my-app.js
@@ -1,9 +1,3 @@
-/*
-
-  Para os imports funcionem, é necessário que no HTML onde é importado
-  o arquivo 'app.js', o parâmetro 'type' tenha o valor 'module'.
-
-*/
 import { checkServiceWorkerAndRegister } from '/webdev/js/pwa-code/checkServiceWorkerAndRegister.js'
 
 const initCode = () => {
@@ -12,14 +6,6 @@ const initCode = () => {
 
 }
 
-/*
-
-  Este evento foi adicionado ao window e faz com que as funções
-  em seu interior só sejam executadas após o load da página, evitando
-  assim que o javascript faça interações com a página antes dela
-  estar pronta para isso.
-
-*/
 window.addEventListener('load', () => {
 
   initCode()
--- webdev/js/pwa-code/fetchServiceWorker.js
+++ webdev/js/pwa-code/fetchServiceWorker.js
@@ -6,26 +6,6 @@ export function fetchServiceWorker() {
 
     const url = new URL(event.request.url)
 
-    /*
-    console.log("### 1")
-    console.log("fetch hello")
-    console.log("fetch event.request.url = ", event.request.url)
-
-    console.log("### 2")
-    console.log("fetch url = ", url)
-    console.log("fetch url.origin = ", url.origin)
-    console.log("fetch location.origin = ", location.origin)
-    console.log("fetch url.pathname = ", url.pathname)
-
-    console.log("### 3")
-
-    const rx = url.origin == location.origin
-    const ry = url.pathname == '/webdev/images/dog.webp'
-
-    console.log("fetch url.origin == location.origin = ", rx)
-    console.log("fetch url.pathname == '/webdev/images/dog.webp' = ", ry)
-    */
-
     if (url.origin == location.origin && url.pathname == '/webdev/images/dog.webp') {
 
       event.respondWith(caches.match('/webdev/images/cat.webp'))
