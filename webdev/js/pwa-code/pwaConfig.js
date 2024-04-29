0a5bd00c1e2fee720cf25599eafef9db98ebe574
Adriano Enache <adrianoenache@hotmail.com>
2024-04-29 11:52:19 -0300

    It was remove parameter from pwa config and changed the version.

--- webdev/js/pwa-code/pwaConfig.js
+++ webdev/js/pwa-code/pwaConfig.js
@@ -1,9 +1,8 @@
 export const appName = 'my-pwa-app'
-export const cacheVersion = appName + '-v-0-0-1'
+export const cacheVersion = appName + '-v-0-0-0'
 export const serviceWorkerPath = '/webdev/serviceworker.js'
 export const serviceWorkerRegisterConfig = {
   type: 'module',
   scope: '/webdev/',
   updateViaCache: 'all'
 }
-export const callTheUpdateOnTheInterval = 2000
